eb538a952471443a96f7a1f4db5c17fa77e3a2b4
h_hajiba <h_hajiba@KHORANA.ENCS.concordia.ca>
2019-01-09 11:30:43 -0500

    Autowire byName, no ambiguity

---
+++ AutoWire_XML_byName/src/com/Tutorial/Hamid/Point.java
@@ -0,0 +1,27 @@
+package com.Tutorial.Hamid;
+
+public class Point {
+	//Attributes
+		private int xPoint;
+		private int yPoint;
+		
+		//Getter and Setter Mothods
+		public int getxPoint() {
+			return xPoint;
+		}
+		public void setxPoint(int xPoint) {
+			this.xPoint = xPoint;
+		}
+		public int getyPoint() {
+			return yPoint;
+		}
+		public void setyPoint(int yPoint) {
+			this.yPoint = yPoint;
+		}
+		
+		//toString Method
+		@Override
+		public String toString() {
+			return "Point [xPoint=" + xPoint + ", yPoint=" + yPoint + "]";
+		}	
+}
---
+++ AutoWire_XML_byName/src/com/Tutorial/Hamid/Triangle.java
@@ -0,0 +1,34 @@
+package com.Tutorial.Hamid;
+
+public class Triangle {
+	//Attributes
+		private Point pointA;
+		private Point pointB;
+		private Point pointC;
+		
+		//Getter and Setter Methods
+		public Point getPointA() {
+			return pointA;
+		}
+		public void setPointA(Point pointA) {
+			this.pointA = pointA;
+		}
+		public Point getPointB() {
+			return pointB;
+		}
+		public void setPointB(Point pointB) {
+			this.pointB = pointB;
+		}
+		public Point getPointC() {
+			return pointC;
+		}
+		public void setPointC(Point pointC) {
+			this.pointC = pointC;
+		}
+		//toString Method
+		@Override
+		public String toString() {
+			return "Triangle [pointA=" + pointA + ", pointB=" + pointB + ", pointC=" + pointC + "]";
+		}
+		
+}
